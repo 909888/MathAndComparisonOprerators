3097c9601540985194780c677bd300088f80a36f
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-08-07 13:14:26 -0700

    Initital upload

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //     using addition
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            // Adding two variables together. Assing a value to a new variable by adding. You can use this for other calculations.
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //      using minus
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus five = " + difference.ToString());
+            //Console.ReadLine;
+
+            //   multiply
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //        divide
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //    an integer divided by an integar is gonna be an integer. it will round to the nearest number. No remaineder.
+            //int quotient = 100 / 17;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+
+            //  this will give you a remainder. Use decimals.
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+
+            //        modulus operator = %. This gives you a remainder. Look it up. 
+            //int remainder 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //an example of using a bool data type. bool= true or false
+            //bool trueorfalse = 12 > 5;
+            //Console.Write(trueorfalse.ToString());
+            //Console.ReadLine();
+
+            //using int in this excercise along with bool data type
+            //int roomTempature = 70;
+            //int currentTempature = 72;
+
+            //bool isWarm = currentTempature > roomTempature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //using different math operators
+            //int roomTempature = 70;
+            //int currentTempature = 72;
+
+            //bool isWarm = currentTempature <= roomTempature;
+            //bool isWarm = currentTempature != roomTempature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            // multiply by 50 example Test 1---
+            //Console.WriteLine("Enter a number: ");
+            //int enterNum = int.Parse(Console.ReadLine());
+
+            //int newNum = 50 * enterNum;
+
+            //Console.WriteLine("The number you picked {0} times {1}", enterNum, newNum);
+            //Console.ReadLine();
+
+
+            //Adding 25 example  Test 2---
+            //Console.WriteLine("Enter a number: ");
+            //int enterNum = int.Parse(Console.ReadLine());
+
+            //int newNum = 25 + enterNum;
+
+            //Console.WriteLine("Now I added 25 to it and now it is, {0}", newNum);
+            //Console.ReadLine();
+
+
+            //  divide a number by 12.5 Test 3---
+            //Console.WriteLine("Enter a number: ");
+            //int enterNum = int.Parse(Console.ReadLine());
+
+            //double newNum = 12.5 / enterNum;
+
+            //Console.WriteLine("Now I shall divide it by 12.5, {0}", newNum);
+            //Console.ReadLine();
+
+            // Equal to fity or nah(true or false) Test 4---
+            //Console.WriteLine("Pick a number and ill tell you if its greater than 50: ");
+            //int inputNum = Convert.ToInt32(Console.ReadLine());
+            //if (inputNum > 50)
+            //{
+            //    Console.WriteLine("True, the number you selected is greater than 50. Great job!!!");
+            // }
+            //else if (inputNum < 50)
+            //{
+            //    Console.WriteLine("False, this number is not greater than 50. TRY AGAIN!!");
+            //}
+            //else if (inputNum == 50) { 
+            //    Console.WriteLine("I mean its not greater than 50, but its equal too. Try it again! Dont fail me!");
+            //}
+            //Console.ReadKey();
+
+            //int Numinput = 7;
+
+
+            //Console.WriteLine("Last thing. Type a number and ill divide by 7 and give you the remainder.");
+            //int inputNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            //getting a remainder by dividing by 7-- - Test 5
+            //Console.WriteLine("Last thing. Type a number and ill divide by 7 and give you the remainder.");
+            //int enterNum = int.Parse(Console.ReadLine());
+
+            //double newNum = enterNum % 7;
+
+            //Console.WriteLine("Now let's take a look and see if that number has a remainder , {0}", newNum);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
